b69dbb181b5b4a38d3aa31b75ce4d9cd5bf96978
NakanoMiku13 <danielulrichtamayo@gmail.com>
2024-10-11 16:16:14 -0600

    Updating code

--- obj/Debug/net8.0/SocketMemory.AssemblyInfo.cs
+++ obj/Debug/net8.0/SocketMemory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocketMemory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efbe4f0c690d743ffb8798189a4bbefc80a4dcf3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+585d31c1197a4d1976eb0c876fa1b2b0cb838f81")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocketMemory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocketMemory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
